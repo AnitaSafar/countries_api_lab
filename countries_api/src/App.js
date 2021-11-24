1ab939526367c00d13ece6882082f73a8353f422
Anita Safar <ancsa0416@gmail.com>
2021-11-24 14:02:56 +0000

    setup done, containers folder created, countriescontainer component created

---
+++ countries_api/src/App.js
@@ -0,0 +1,10 @@
+import CountriesContainer from './containers/CountriesContainer';
+import './App.css';
+
+function App() {
+  return (
+    <CountriesContainer/>
+  );
+}
+
+export default App;
---
+++ countries_api/src/containers/CountriesContainer.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+const CountriesContainer = () => {
+    const [countries, setCountries] = useState([]);
+
+    
+
+    return(
+        <h1>Countries</h1>
+    );
+};
+
+export default CountriesContainer;
